8219d57f2ce0a101c8ff3a11408e6810731b9ca2
C0MODIN <38263591+C0MODIN@users.noreply.github.com>
2023-04-02 14:28:10 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # vsftpd_systrayapp
+
+## Install option
+
+1. Assign execute permission.
+```
+chmod +x install.sh
+```
+2. Run the script.
+```
+./install.sh
+```
---
+++ install.sh
@@ -0,0 +1,20 @@
+ #!/bin/bash
+
+if [ -d /opt/vsftpd_systrayapp ]; then
+    echo "..."
+else
+    sudo mkdir /opt/vsftpd_systrayapp
+    echo "Directory in /opt created"
+fi
+
+if [ -f /usr/share/applications/vsftpdkde.desktop ]; then
+    echo "..."
+else
+    sudo touch /usr/share/applications/vsftpdkde.desktop
+    echo "Desktop file created"
+fi
+
+sudo cp -r icons/ main.py /opt/vsftpd_systrayapp && sudo cp vsftpdkde.desktop /usr/share/applications/VSFTPDKDE && sudo chmod +x /opt/vsftpd_systrayapp/main.py && sudo chmod a+r /usr/share/applications/vsftpdkde.desktop
+if [ $? -eq 0 ]; then
+    echo "Installation succesfully"
+fi
