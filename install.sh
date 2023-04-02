1c43a3d7bb2179d1abc2c4167ca887de93445b4c
C0MODIN <jonatanayllon@gmail.com>
2023-04-02 15:55:55 -0300

    first commit

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
- #!/bin/bash
+#!/bin/bash
 
+# Create /opt/vsftpd_systrayapp folder
 if [ -d /opt/vsftpd_systrayapp ]; then
     echo "..."
 else
@@ -7,6 +8,15 @@ else
     echo "Directory in /opt created"
 fi
 
+# Create ~/.local/share/icons/hicolor/48x48/icons_vsftpdkde folder
+if [ -d ~/.local/share/icons/hicolor/48x48/icons_vsftpdkde ]; then
+    echo "..."
+else
+    sudo mkdir /opt/vsftpd_systrayapp
+    echo "Directory in ~/.local/share/icons/hicolor/48x48/icons_vsftpdkde created"
+fi
+
+# Create /usr/share/applications/vsftpdkde.desktop folder
 if [ -f /usr/share/applications/vsftpdkde.desktop ]; then
     echo "..."
 else
@@ -14,7 +24,7 @@ else
     echo "Desktop file created"
 fi
 
-sudo cp -r icons/ main.py /opt/vsftpd_systrayapp && sudo cp vsftpdkde.desktop /usr/share/applications/vsftpdkde.desktop && sudo chmod +x /opt/vsftpd_systrayapp/main.py && sudo chmod a+r /usr/share/applications/vsftpdkde.desktop && sudo ln -s /opt/vsftpd_systrayapp/main.py /usr/local/bin/vsftpdkde
+sudo cp -r vsftpdkde.py /opt/vsftpd_systrayapp && sudo cp -r icons_vsftpdkde/ ~/.local/share/icons/hicolor/48x48 && sudo cp vsftpdkde.desktop /usr/share/applications/vsftpdkde.desktop && sudo chmod +x /opt/vsftpd_systrayapp/vsftpdkde.py && sudo chmod a+r /usr/share/applications/vsftpdkde.desktop && sudo ln -s /opt/vsftpd_systrayapp/vsftpdkde.py /usr/local/bin/vsftpdkde
 if [ $? -eq 0 ]; then
     echo "Installation succesfully"
 fi
